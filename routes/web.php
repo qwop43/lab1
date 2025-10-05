0377ffd06c333937a9954bc1c2c7b5ff1fa74671
qwop43 <dimasiva58@gmail.com>
2025-10-05 10:47:08 +0000

    feat(routes): add routes with query.mode middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LabController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['query.mode'])->group(function () {
+    Route::get('/', [LabController::class, 'index'])->name('home');
+    Route::get('/about', [LabController::class, 'about'])->name('about');
+    Route::get('/status', [LabController::class, 'status'])->name('status');
+    Route::get('/echo', [LabController::class, 'echo'])->name('echo');
 });

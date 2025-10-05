bb8a31b6132e8ca62c118701dae226839443cd76
qwop43 <dimasiva58@gmail.com>
2025-10-05 11:23:49 +0000

    feat(middleware): add QueryModeMiddleware

--- app/Http/Middleware/QueryModeMiddleware.php
+++ app/Http/Middleware/QueryModeMiddleware.php
@@ -8,13 +8,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class QueryModeMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        $mode = $request->query('mode');
+        
+        if ($mode === 'debug') {
+            // Додаткові дії для debug режиму
+            \Log::info('Debug mode activated for URL: ' . $request->fullUrl());
+        }
+        
         return $next($request);
     }
 }
